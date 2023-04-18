c9460ccc3fbad311a227a7809df5c12c452a8771
kimhyojung <rlgywnd123@gmail.com>
2023-04-19 01:27:57 +0900

    Feat: AboutMe 구현중, 반응형 기초작업

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,19 +1,18 @@
-import { Link } from 'react-router-dom';
 import * as S from './style';
 import Layout from 'components/Layout';
 import Skills from '../../components/Skills';
-import About from 'components/About';
+// import About from 'components/About';
 import AboutMe from 'components/About/AboutMe';
+import React from 'react';
+// import SideBar from 'components/Layout/SideBar';
+import Projects from '../../components/Projects';
 
 const Main = () => {
-  const components = [<About />, <AboutMe />, <Skills />];
+  const components = [<AboutMe />, <Skills />, <Projects />];
 
   return (
     <S.MainContainer>
-      <Layout children={components}>
-        {/* <div>여기는 메인 페이지!</div>
-        <Link to={'/'}>Home으로 가기</Link> */}
-      </Layout>
+      <Layout children={React.Children.toArray(components)}></Layout>
     </S.MainContainer>
   );
 };
--- src/pages/main/style.js
+++ src/pages/main/style.js
@@ -4,5 +4,10 @@ export const MainContainer = styled.div`
   display: flex;
   width: 100%;
   height: 100%;
-  border: 1px solid red;
+  border: 1px solid green;
 `;
+// export const SideBarAddComponentDiv = styled.div`
+//   display: flex;
+//   padding: 0 300px 0 70px;
+//   gap: 70px;
+// `;
