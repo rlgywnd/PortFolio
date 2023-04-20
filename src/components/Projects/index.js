12d863feec1a274b3745e16c6cb604c1963aecc4
kimhyojung <rlgywnd123@gmail.com>
2023-04-20 23:14:48 +0900

    Feat: Projects 컴포 구현중

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,10 +1,20 @@
 import * as S from './style';
+import { RxCheck } from 'react-icons/rx';
+import { RiCheckboxBlankCircleFill } from 'react-icons/ri';
+import uyou from '../../assets/uyou.mp4';
+import { MdOutlineKeyboardArrowDown } from 'react-icons/md';
+import { useState } from 'react';
 
 const Projects = () => {
+  const [uyouMain, setUyouMain] = useState(false);
+  const [uyouSign, setUyouSign] = useState(false);
+  const [uyouLogin, setUyouLogin] = useState(false);
+  const [uyouMyPage, setUyouMyPage] = useState(false);
+
   return (
     <S.ProjectsContainer id='projects'>
       <S.TitleUnderlineDiv>
-        <S.ProjectsTitleH1>PROJECTS</S.ProjectsTitleH1>
+        <S.ProjectsTitleH1>PROJECTS.</S.ProjectsTitleH1>
       </S.TitleUnderlineDiv>
       <S.ProjectsDiv>
         <S.PortFolioDiv>
@@ -13,28 +23,154 @@ const Projects = () => {
         </S.PortFolioDiv>
         <S.UyouBooDanDiv>
           <S.ProjectsTypeDiv>팀 프로젝트</S.ProjectsTypeDiv>
-          <S.ProjectsNameDiv>우유부단</S.ProjectsNameDiv>
-          <div>
-            <div>
-              <div>프로젝트 소개</div>
-              <div>일상 속 결정장애를 위한 공개 투표 서비스</div>
-            </div>
-            <div>
-              <div>img,gif</div>
-            </div>
-            <div>
-              <div>레이아웃,회원가입,로그인,로그아웃,마이페이지</div>
-              <div></div>
-              <div></div>
-              <div></div>
-            </div>
-          </div>
-          <div>
-            <div>사용기술</div>
-            <div>React</div>
+          <S.ProjectsNameDiv>우유부단 : 공개 투표 서비스</S.ProjectsNameDiv>
+          <S.IntroAndWorkDetail>
+            <S.IntroDateDiv>
+              <S.WorkDate>2023년 1월 - 진행중</S.WorkDate>
+              <S.IntroDiv>
+                <h1>나의 작업 내용</h1>
+                <div>
+                  <RxCheck size='30' color='rgb(248, 205, 7)' />
+                  {/* <RiCheckboxBlankCircleFill
+                    size='10'
+                    color='rgb(248, 205, 7)'
+                  /> */}
+                  레이아웃 - 컴포넌트에 공통으로 적용될 레이아웃 틀 구현
+                </div>
+                <div>
+                  <RiCheckboxBlankCircleFill
+                    size='10'
+                    color='rgb(248, 205, 7)'
+                  />
+                  회원가입 - 유효성 & 중복검사를 통한 회원가입 기능 구현
+                </div>
+                <div>
+                  <RiCheckboxBlankCircleFill
+                    size='10'
+                    color='rgb(248, 205, 7)'
+                  />
+                  로그인 - 일반로그인시 Window저장객체에 따른 로그인유지 기능
+                  구현
+                </div>
+                <div>
+                  <RiCheckboxBlankCircleFill
+                    size='10'
+                    color='rgb(248, 205, 7)'
+                  />
+                  로그인 - 소셜로그인 기능 구현 (카카오 & 구글)
+                </div>
+                <div>
+                  <RiCheckboxBlankCircleFill
+                    size='10'
+                    color='rgb(248, 205, 7)'
+                  />
+                  마이페이지 - 내정보수정 & 나의 [투표, 댓글, 게시글] 보기 기능
+                  구현
+                </div>
+              </S.IntroDiv>
+            </S.IntroDateDiv>
+            <S.VideoTitleDiv>실제 사용모습</S.VideoTitleDiv>
+            <S.VideoContainer>
+              <S.VideoTextAndBtn
+                uyoumain={uyouMain}
+                onClick={() => {
+                  setUyouMain(!uyouMain);
+                }}
+              >
+                <S.VideoText>메인화면</S.VideoText>
+                <S.VideoUyouMainBtnDiv uyoumain={uyouMain}>
+                  <MdOutlineKeyboardArrowDown size='25' color='#667085' />
+                </S.VideoUyouMainBtnDiv>
+              </S.VideoTextAndBtn>
+              <S.VideoUyouMainDiv uyoumain={uyouMain}>
+                <S.UyouMainVideo
+                  controls='controls'
+                  src={uyou}
+                  alt='빔'
+                ></S.UyouMainVideo>
+              </S.VideoUyouMainDiv>
+              {/*  */}
+              <S.VideoTextAndBtn
+                uyousign={uyouSign}
+                onClick={() => {
+                  setUyouSign(!uyouSign);
+                }}
+              >
+                {console.log('uyouSign : ', uyouSign)}
+                <S.VideoText>회원가입화면</S.VideoText>
+                <S.VideoUyouSignBtnDiv uyousign={uyouSign}>
+                  <MdOutlineKeyboardArrowDown size='25' color='#667085' />
+                </S.VideoUyouSignBtnDiv>
+              </S.VideoTextAndBtn>
+              <S.VideoUyouSignDiv uyousign={uyouSign}>
+                <S.UyouSignVideo
+                  controls='controls'
+                  src={uyou}
+                  alt='빔'
+                ></S.UyouSignVideo>
+              </S.VideoUyouSignDiv>
+              {/*  */}
+              <S.VideoTextAndBtn
+                uyoulogin={uyouLogin}
+                onClick={() => {
+                  setUyouLogin(!uyouLogin);
+                }}
+              >
+                <S.VideoText>로그인화면</S.VideoText>
+                <S.VideoUyouLoginBtnDiv uyoulogin={uyouLogin}>
+                  <MdOutlineKeyboardArrowDown size='25' color='#667085' />
+                </S.VideoUyouLoginBtnDiv>
+              </S.VideoTextAndBtn>
+              <S.VideoUyouLoginDiv uyoulogin={uyouLogin}>
+                <S.UyouLoginVideo
+                  controls='controls'
+                  src={uyou}
+                  alt='빔'
+                ></S.UyouLoginVideo>
+              </S.VideoUyouLoginDiv>
+              {/*  */}
+              <S.VideoTextAndBtn
+                uyoumypage={uyouMyPage}
+                onClick={() => {
+                  setUyouMyPage(!uyouMyPage);
+                }}
+              >
+                <S.VideoText>마이페이지화면</S.VideoText>
+                <S.VideoUyouMyPageBtnDiv uyoumypage={uyouMyPage}>
+                  <MdOutlineKeyboardArrowDown size='25' color='#667085' />
+                </S.VideoUyouMyPageBtnDiv>
+              </S.VideoTextAndBtn>
+              <S.VideoUyouMyPageDiv uyoumypage={uyouMyPage}>
+                <S.UyouMyPageVideo
+                  controls='controls'
+                  src={uyou}
+                  alt='빔'
+                ></S.UyouMyPageVideo>
+              </S.VideoUyouMyPageDiv>
+            </S.VideoContainer>
+          </S.IntroAndWorkDetail>
+          {/*  */}
+          {/*  */}
+          <S.SkillsTitleDiv>사용기술</S.SkillsTitleDiv>
+          <S.SkillsContainer>
+            <S.SkillsTextDiv>
+              <RiCheckboxBlankCircleFill size='10' color='rgb(248, 205, 7)' />
+              React
+            </S.SkillsTextDiv>
+            <S.SkillsDetailDiv>
+              UI 개발에 집중할 수 있습니다. 컴포넌트를 이용해서 재사용 가능하고,
+              논리적으로 파일을 나누어서 개발 진행을 할 수 있습니다. 또한 JSX
+              문법을 이용해서 조금 더 직관적으로 템플릿을 구성할 수 있어서
+              사용했습니다. 그리고 커뮤니티가 방대해서 자료 찾기도 편합니다.
+            </S.SkillsDetailDiv>
+
+            {/*  */}
             <div>Typescript</div>
-            <div>Next.js</div>
+            {/*  */}
+            <div>Next.JS</div>
+            {/*  */}
             <div>Redux</div>
+            <div>MSW</div>
             <div>Redux-ToolKit</div>
             <div>Styled-Components</div>
             <div>React-hook-form</div>
@@ -42,8 +178,18 @@ const Projects = () => {
             <div>ESLint</div>
             <div>Prettier</div>
             <div>Styled-reset</div>
+          </S.SkillsContainer>
+          <div>
+            <div>관련링크</div>
+            <div>
+              <h5>GIT</h5>
+              <div>깃헙URL</div>
+            </div>
+            <div>
+              <h5>관련 블로깅</h5>
+              <div>에러핸들링 등 내 블로그 url</div>
+            </div>
           </div>
-          <div>관련링크</div>
         </S.UyouBooDanDiv>
         <S.BoBeDiv>
           <S.ProjectsTypeDiv>팀 프로젝트</S.ProjectsTypeDiv>
--- src/components/Projects/style.js
+++ src/components/Projects/style.js
@@ -2,7 +2,6 @@ import styled from 'styled-components';
 
 export const ProjectsContainer = styled.div`
   flex: 1;
-  /* border: 1px solid blue; */
 `;
 export const ProjectsTitleH1 = styled.h1`
   display: flex;
@@ -28,13 +27,213 @@ export const ProjectsDiv = styled.div``;
 
 export const ProjectsTypeDiv = styled.div`
   font-size: 25px;
+  font-family: 'NEXON Lv2 Gothic' !important;
   margin-bottom: 20px;
 `;
 export const ProjectsNameDiv = styled.div`
   font-size: 40px;
   font-weight: bold;
+  margin-bottom: 20px;
+`;
+export const IntroAndWorkDetail = styled.div`
+  div {
+    font-family: 'NEXON Lv2 Gothic' !important;
+  }
+`;
+export const WorkDate = styled.div`
+  margin-bottom: 20px;
+  color: #667085;
+  font-weight: bold;
+`;
+export const IntroDateDiv = styled.div`
+  margin-bottom: 20px;
+`;
+export const IntroDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  h1 {
+    font-size: 20px;
+    font-family: 'NEXON Lv2 Gothic' !important;
+  }
+  div {
+    display: flex;
+    align-items: center;
+    gap: 10px;
+    font-size: 18px;
+    font-weight: bold;
+  }
+`;
+export const VideoTitleDiv = styled.div`
+  font-size: 20px;
+  font-family: 'NEXON Lv2 Gothic' !important;
+  margin-bottom: 20px;
 `;
 
+// 실제사용모습들 모두를 담을 컨테이너 div
+export const VideoContainer = styled.div`
+  width: 100%;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 20px;
+`;
+
+// 동영상나오기전 위에 text랑 화살표모양 담은 박스
+export const VideoTextAndBtn = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: white;
+  height: 45px;
+  padding: 0 15px 0 15px;
+  cursor: pointer;
+  /* border: 1px solid red; */
+`;
+
+// text div  ex -> 메인화면, 로그인화면 등
+export const VideoText = styled.div`
+  font-size: 14px;
+`;
+
+// 화살표모양을 담은 div
+export const VideoUyouMainBtnDiv = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  transition: transform 0.3s ease 0s;
+  transform: ${(props) => (props.uyoumain ? 'rotate(180deg)' : '')};
+`;
+export const VideoUyouSignBtnDiv = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  transition: transform 0.3s ease 0s;
+  transform: ${(props) => (props.uyousign ? 'rotate(180deg)' : '')};
+`;
+export const VideoUyouLoginBtnDiv = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  transition: transform 0.3s ease 0s;
+  transform: ${(props) => (props.uyoulogin ? 'rotate(180deg)' : '')};
+`;
+export const VideoUyouMyPageBtnDiv = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  transition: transform 0.3s ease 0s;
+  transform: ${(props) => (props.uyoumypage ? 'rotate(180deg)' : '')};
+`;
+
+// video를 담은 div
+export const VideoUyouMainDiv = styled.div`
+  width: 100%;
+  box-sizing: border-box;
+  overflow: hidden;
+  transition: max-height 0.3s linear;
+  max-height: 0;
+  max-height: ${(props) => (props.uyoumain ? '100vh' : '0')};
+  margin-bottom: 7px;
+`;
+export const VideoUyouSignDiv = styled.div`
+  width: 100%;
+  box-sizing: border-box;
+  overflow: hidden;
+  transition: max-height 0.3s linear;
+  max-height: 0;
+  max-height: ${(props) => (props.uyousign ? '100vh' : '0')};
+  margin-bottom: 7px;
+`;
+export const VideoUyouLoginDiv = styled.div`
+  width: 100%;
+  box-sizing: border-box;
+  overflow: hidden;
+  transition: max-height 0.3s linear;
+  max-height: 0;
+  max-height: ${(props) => (props.uyoulogin ? '100vh' : '0')};
+  margin-bottom: 7px;
+`;
+export const VideoUyouMyPageDiv = styled.div`
+  width: 100%;
+  box-sizing: border-box;
+  overflow: hidden;
+  transition: max-height 0.3s linear;
+  max-height: 0;
+  max-height: ${(props) => (props.uyoumypage ? '100vh' : '0')};
+  margin-bottom: 7px;
+`;
+
+// video태그 자체
+export const UyouMainVideo = styled.video`
+  width: 100%;
+  height: 100%;
+  padding: 15px;
+  padding: ${(props) => (props.uyoumain ? '15px' : '0')};
+  margin-bottom: ${(props) => (props.uyoumain ? '30px' : '0px')};
+  background-color: white;
+`;
+export const UyouSignVideo = styled.video`
+  width: 100%;
+  height: 100%;
+  padding: 15px;
+  padding: ${(props) => (props.uyousign ? '15px' : '0')};
+  margin-bottom: ${(props) => (props.uyousign ? '30px' : '0px')};
+  background-color: white;
+`;
+export const UyouLoginVideo = styled.video`
+  width: 100%;
+  height: 100%;
+  padding: 15px;
+  padding: ${(props) => (props.uyoulogin ? '15px' : '0')};
+  margin-bottom: ${(props) => (props.uyoulogin ? '30px' : '0px')};
+  background-color: white;
+`;
+export const UyouMyPageVideo = styled.video`
+  width: 100%;
+  height: 100%;
+  padding: 15px;
+  padding: ${(props) => (props.uyoumypage ? '15px' : '0')};
+  margin-bottom: ${(props) => (props.uyoumypage ? '30px' : '0px')};
+  background-color: white;
+`;
+
+// 사용기술 제목을 담을 div
+export const SkillsTitleDiv = styled.div`
+  font-size: 20px;
+  font-family: 'NEXON Lv2 Gothic' !important;
+  margin-bottom: 20px;
+`;
+// 사용기술을 담을 최상 컨테이너 div
+export const SkillsContainer = styled.div`
+  width: 100%;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 20px;
+  font-size: 20px;
+  font-family: 'NEXON Lv2 Gothic' !important;
+`;
+
+// Skills 이름, 점 img 감쌀 Text
+export const SkillsTextDiv = styled.div`
+  font-size: 20px;
+  font-family: 'NEXON Lv2 Gothic' !important;
+  display: flex;
+  align-items: center;
+  gap: 10px;
+  font-weight: bolder;
+  margin-bottom: 10px;
+`;
+
+export const SkillsDetailDiv = styled.div`
+  /* border: 1px solid red; */
+  font-size: 18px;
+  font-family: 'NEXON Lv2 Gothic' !important;
+  padding: 0 0 0 18px;
+`;
+
+// 각 프로젝트 최상 Div
 export const PortFolioDiv = styled.div`
   margin-bottom: 50px;
 `;
