bdbfa23abc0df9231dafd023ab25bdf46311a30d
kimhyojung <rlgywnd123@gmail.com>
2023-05-08 19:23:40 +0900

    URL 코드 파일 분리

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -17,33 +17,25 @@ import bobemypage from '../../assets/bobemypage.mp4';
 
 import { useState } from 'react';
 
+import { portGitUrl, portBlogUrl } from 'constants/url';
+import {
+  uyouBlogUrl,
+  uyouGitUrl,
+  uyouWorkBlogUrl,
+  uyouDeployUrl,
+  uyouNaverBlogUrl,
+} from 'constants/url';
+import {
+  bobeDeployUrl,
+  bobeBlogUrl,
+  bobeGitUrl,
+  bobeNotionUrl,
+  bobeInterViewUrl,
+  bobeWorkBlogUrl,
+} from 'constants/url';
+import { padakDemoUrl, padakGitUrl, padakDeployUrl } from 'constants/url';
+
 const Projects = () => {
-  // url 링크
-  const uyouDeployUrl =
-    'http://uyouboodan-bucket-deploy.s3-website.ap-northeast-2.amazonaws.com/';
-  const uyouGitUrl = 'https://github.com/Team-Big-Dipper/UYouBooDan/tree/fe';
-  const uyouBlogUrl =
-    'https://velog.io/@hyo123/Next.js-localStorage-%EC%97%90%EB%9F%AC%ED%95%B8%EB%93%A4%EB%A7%81';
-  const uyouWorkBlogUrl =
-    'https://velog.io/@hyo123/%EC%9A%B0%EC%9C%A0%EB%B6%80%EB%8B%A8-%EB%A0%88%EC%9D%B4%EC%95%84%EC%9B%83';
-  const uyouNaverBlogUrl =
-    'https://velog.io/@hyo123/%EB%84%A4%EC%9D%B4%EB%B2%84-%EC%86%8C%EC%85%9C-%EB%A1%9C%EA%B7%B8%EC%9D%B8';
-  const bobeGitUrl = 'https://github.com/codestates-seb/seb40_main_030';
-  const bobeNotionUrl =
-    'https://www.notion.so/codestates/8e43f8220bae4542b0ae84d8caa06b5a';
-  const bobeInterViewUrl = 'https://www.youtube.com/watch?v=yOCwQrxzy6U';
-  const bobeDeployUrl =
-    'http://battery-bucket-deploy.s3-website.ap-northeast-2.amazonaws.com/';
-  const bobeBlogUrl = 'https://velog.io/@hyo123/%EB%A1%9C%EA%B7%B8%EC%9D%B8';
-  const bobeWorkBlogUrl =
-    'https://velog.io/@hyo123/%EB%B3%B4%EB%B0%B0%EB%B9%8C%EB%A6%BC-%EC%9E%91%EC%97%85%EB%82%B4%EC%9A%A9%EC%A0%95%EB%A6%AC';
-  const padakDeployUrl =
-    'http://padakmon-client-bucket.s3-website.ap-northeast-2.amazonaws.com/';
-  const padakGitUrl = 'https://github.com/codestates-seb/seb40_pre_003';
-  const padakDemoUrl = 'https://www.youtube.com/watch?v=T6uEMcfgvec';
-  const portGitUrl = 'https://github.com/rlgywnd/PortFolio';
-  const portBlogUrl =
-    'https://velog.io/@hyo123/Deploy-AWS-S3-%EC%A0%95%EC%A0%81-%EC%9B%B9-%ED%98%B8%EC%8A%A4%ED%8C%85';
   // 구현모습쪽 버튼 열고닫기 state
   const [uyouMain, setUyouMain] = useState(false);
   const [uyouSign, setUyouSign] = useState(false);
