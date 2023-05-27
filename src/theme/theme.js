6ac1d64b12ea915a0283817f5c9d302e2d005141
kimhyojung <rlgywnd123@gmail.com>
2023-05-28 01:27:15 +0900

    다크모드 색상 변경

--- src/theme/theme.js
+++ src/theme/theme.js
@@ -3,7 +3,7 @@ export const lightTheme = {
   textColor: 'black',
 };
 export const darkTheme = {
-  bgColor: 'black',
+  bgColor: '#1E1E21',
   textColor: 'rgb(245, 245, 245)',
 };
 export const theme = {
