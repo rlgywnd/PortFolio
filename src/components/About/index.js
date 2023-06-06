8bf446c41411538616829c412bdbb238714ceedc
kimhyojung <rlgywnd123@gmail.com>
2023-06-07 08:23:58 +0900

    수정

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -9,7 +9,7 @@ const About = () => {
     <S.AboutContainer id='about'>
       <S.ShortAboutDiv isdark={isDark}>
         <S.ShortAboutSection isdark={isDark}>
-          <div>이해될 때까지 하는</div>
+          <div>기록하며 배우는</div>
           <div>
             <span>김효중</span>
             <div>입니다.</div>
@@ -17,7 +17,7 @@ const About = () => {
         </S.ShortAboutSection>
         <S.ShortAboutTextDiv>
           <div>매번 왜? 라 생각하며 이해될 때까지</div>
-          <div>학습하여 내 것으로 만드는 편입니다.</div>
+          <div>기록하며 학습하는걸 좋아합니다.</div>
           <div>뒤처지는 거 싫어합니다. 언제나 배우며</div>
           <div>포기않고 계속 성장하고 싶습니다.</div>
         </S.ShortAboutTextDiv>
